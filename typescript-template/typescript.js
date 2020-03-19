bacb35504449da00d8630e140681116cc864b1cc
talaat197 <talaatmedhat19@gmail.com>
2020-03-19 20:18:26 +0200

    types

--- typescript-template/typescript.js
+++ typescript-template/typescript.js
@@ -1,2 +1,31 @@
+"use strict";
 // boolean type
 var isGood = true;
+var sum = function (x, y) {
+    return x + y;
+};
+//number
+//Tuple
+var basket;
+basket = ['hello', 6];
+// enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+var sizeName = Size[3]; // would print Large
+var sizeNum = Size.Large; // would print  3
+var anything = "nooo";
+anything = 5;
+anything = "yes";
+anything = true;
+// void
+var dance = function () {
+    console.log('dance with me');
+};
+var error = function () {
+    throw Error('ops');
+};
--- typescript-template/typescript.ts
+++ typescript-template/typescript.ts
@@ -1,4 +1,32 @@
 // boolean type
 let isGood: boolean = true;
 
+const sum = (x: number , y:number) : number => {
+    return x+y;
+}
 //number
+
+//Tuple
+let basket: [string , number]
+basket = ['hello' , 6];
+
+// enum
+enum Size { Small = 1 , Medium = 2 , Large = 3};
+
+let sizeName : string  = Size[3];   // would print Large
+
+let sizeNum : number = Size.Large;  // would print  3
+
+let anything : any = "nooo";
+anything = 5;
+anything = "yes";
+anything = true;
+
+// void
+let dance = () : void => {
+    console.log('dance with me');
+}
+
+let error = () : never => {
+    throw Error('ops');
+}
