396be1801b781578ecb1514645d9f4b32d080156
talaat197 <talaatmedhat19@gmail.com>
2020-04-29 23:52:30 +0200

    lambda function

---
+++ serverless-service/handler.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports.tmt_lambda = async event => {
+  const emoijs = [
+    ':)' , ':D' , ';)' , '<3'
+  ]
+  let rank = 0;
+
+  if(event.queryStringParameters && event.queryStringParameters.rank)
+  {
+    rank = event.queryStringParameters.rank;
+    rank = rank >= emoijs.length ? emoijs.length - 1 : rank;
+  }
+   
+  const rankEmoji = emoijs[rank];
+  
+  return {
+    statusCode: 200, 
+    header : {
+      'Access-Control-Allow-Origin' : "*"
+    },
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+        rank : rankEmoji
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
