dca83cc8a1cc5a44ea09c71408ad1d392747043a
talaat197 <talaatmedhat19@gmail.com>
2021-11-08 12:51:35 +0200

    should not commit

---
+++ robo-template/scripts.test.js
@@ -0,0 +1,9 @@
+
+
+// group your test under a certain title
+describe('google search' , () => {
+    test('search in google engine' , () => {
+        // expect('hello').toBe('No');
+        expect(1).toEqual(1);
+    });
+})
